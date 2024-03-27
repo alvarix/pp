1de4f727ef8e4b000c4b67cd79f8cfd61cca7ff7
= <=>
2024-03-26 21:21:57 -0400

    finish link list and forms

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-
-thumb_gallery = () => {
+// create ul of thumbs
+thumb_gallery = (id) => {
     const n = 25;
-    const ul = getElementById("thumbs");
+    const ul = document.getElementById(id);
+    const imgPaths = [];
 
     for (let i = 1; i <= n; i++) {
+        imgPaths.push("img/thumb-" + i + ".png");
+    }
+    shuffleArray(imgPaths);
+    imgPaths.forEach(path => {
         const li = document.createElement("li");
         const img = document.createElement("img");
-        
-        img.src = "img/thumb-"; // Set the image source here
-        
+        img.src = path;
         li.appendChild(img);
-        li.appendChild(document.createTextNode("Image " + i));
-        
         ul.appendChild(li);
+    });
+}
+
+// shuffle onload
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// periodic reshuffle
+function shuffleList(id) {
+    var list = document.getElementById(id);
+    for (var i = list.children.length; i >= 0; i--) {
+      list.appendChild(list.children[Math.random() * i | 0]);
     }
   }
+ 
+
+// id of gallery
+const id ='thumbs';
+thumb_gallery(id);
+setInterval(shuffleList(id), 1000);
